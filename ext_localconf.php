c9463d9745141bf8b1b544dcfaafd17474583999
aoemedia <aoemedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-11-09 14:41:30 +0000

    make disable cookie optional
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cacheinfo/trunk@53952 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,10 @@
 
 //include because it needs to be prefixed with user_
 require_once(t3lib_extMgm::extPath('cacheinfo').'Classes/Hooks/Userauth.php');
-
-$TYPO3_CONF_VARS['FE']['dontSetCookie'] = TRUE;    // we don't wantt set the fe_user cookie by default
+$conf = unserialize ( $GLOBALS ['TYPO3_CONF_VARS'] ['EXT'] ['extConf'] ['cacheinfo'] );
+if (isset($conf['setOnlyCookieForLogin']) && $conf['setOnlyCookieForLogin'] == 1) {
+	$TYPO3_CONF_VARS['FE']['dontSetCookie'] = TRUE;    // we don't wantt set the fe_user cookie by default
+}
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'user_Tx_Cacheinfo_Hooks_Userauth->writeLoginSessionCookie';
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['cacheinfo'] = 'EXT:cacheinfo/Classes/Hooks/SendCacheDebugHeader.php:&tx_Cacheinfo_Hooks_SendCacheDebugHeader->sendCacheDebugHeader';
 
