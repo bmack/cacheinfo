cb2fd523f0daec45dca5ca6de619b70ebc824349
Benjamin Mack <benni@typo3.org>
2014-04-28 16:04:15 +0200

    [TASK] Only add cookie option for 6.1 and below

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,13 +4,17 @@ if (!defined ('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$extensionConfiguration = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['cacheinfo']);
+// "only set cookie when logged in" works in 6.2 out-of-the-box
+if (version_compare('6.2', TYPO3_branch, '>')) {
 
-// we don't want set the fe_user cookie by default
-if (!is_array($extensionConfiguration) || !empty($extensionConfiguration['setOnlyCookieForLogin'])) {
-	$TYPO3_CONF_VARS['FE']['dontSetCookie'] = TRUE;
+	$extensionConfiguration = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['cacheinfo']);
+	// we don't want set the fe_user cookie by default
+	if (!is_array($extensionConfiguration) || !empty($extensionConfiguration['setOnlyCookieForLogin'])) {
+		$TYPO3_CONF_VARS['FE']['dontSetCookie'] = TRUE;
+	}
+
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['cacheinfo'] = 'AOE\\Cacheinfo\\Hooks\\SendCacheDebugHeader->sendCacheDebugHeader';
 }
 
-// register hooks
+// register hook for sending cookie
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'AOE\\Cacheinfo\\Hooks\\Userauth->writeLoginSessionCookie';
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['cacheinfo'] = 'AOE\\Cacheinfo\\Hooks\\SendCacheDebugHeader->sendCacheDebugHeader';
