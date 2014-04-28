75fc70a54902a4a35340932ae759d8f1f5d4d154
Benjamin Mack <benni@typo3.org>
2014-04-28 10:18:40 +0200

    [TASK] Update Extension version

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,11 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Cacheinfo',
+	'title' => 'Cache Info',
 	'description' => 'Adjust server responses and adds cache debug headers to make more pages cachable (e.g. varnish or nginx)',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearcacheonload' => 1,
 	'lockType' => '',
-	'author' => '',
-	'author_email' => '',
+	'author' => 'AOE and Benni Mack',
+	'author_email' => 'benni@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
@@ -29,7 +29,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'purge' => ''
 		),
 	),
 	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"1df2";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"850a";s:8:"ext_icon";s:4:"4660";s:12:"ext_icon.gif";s:4:"4660";s:17:"ext_localconf.php";s:4:"b3b3";s:14:"ext_tables.php";s:4:"fef1";s:14:"ext_tables.sql";s:4:"d41d";s:18:"user_renderInt.php";s:4:"a5aa";s:14:"doc/manual.sxw";s:4:"8fe7";s:19:"doc/wizard_form.dat";s:4:"abce";s:20:"doc/wizard_form.html";s:4:"77a9";s:40:"lib/tx_mocvarnish_tcemain_cachehooks.php";s:4:"c22d";s:18:"lib/URL/Finder.php";s:4:"ade9";s:28:"lib/Varnish/CacheManager.php";s:4:"5975";s:16:"static/setup.txt";s:4:"dc83";s:29:"vcl-examples/esi-forced-5.vcl";s:4:"25da";s:25:"vcl-examples/esi-full.vcl";s:4:"7063";s:38:"vcl-examples/esi-header-controlled.vcl";s:4:"301d";s:25:"vcl-examples/esi-test.vcl";s:4:"1f81";s:33:"xclass/class.ux_tslib_content.php";s:4:"78d0";s:28:"xclass/class.ux_tslib_fe.php";s:4:"3c1e";}',
